d296f9b29883b86c43a9205d1aa828f03cb6210a
CHONKED MAGGOT <>
2022-09-25 22:06:27 +0000

    Will message in channel price and steam community market url

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def on_ready() :
 
 #, weapon, wear, statTrak, Souvenir, *args
 @client.command()
-async def searchMarket(ctx, weapon, condition, statTrakAndSouvenir, *args) :
+async def searchMarket(ctx, condition, statTrakAndSouvenir, weapon, *args) :
 	if int(statTrakAndSouvenir) == 1 :
 		print("true")
 		search = "https://steamcommunity.com/market/listings/730/StatTrak™%20"
@@ -21,18 +21,25 @@ async def searchMarket(ctx, weapon, condition, statTrakAndSouvenir, *args) :
 		search = "https://steamcommunity.com/market/listings/730/Souvenir%20"
 	else :
 		search ="https://steamcommunity.com/market/listings/730/"
+
+	item = searchMod.replaceWeaponName(weapon)[1] + " | "
 	print(search)
 	#if statTrak == 1 :
 	#	search = search + "StatTrak™%20"
-	search = search + searchMod.replaceWeaponName(weapon) + "%7C%20"
+	search = search + searchMod.replaceWeaponName(weapon)[0] + "%7C%20"
 	#%28Factory%20New%29
 	print(search)
 	for word in args :
 		search = search + word + "%20"
-	link = search + searchMod.replaceWeaponWear(condition)
+		item = item + word + " "
+	link = search + searchMod.replaceWeaponWear(condition)[0]
+	item = item + searchMod.replaceWeaponWear(condition)[1]
 	print(link)
-	item = steam.get_csgo_item("AK-47 | Frontside Misty (Factory New)", currency='USD')
+	price = steam.get_csgo_item(item, currency='USD')
 	print(item)
+	print(price)
+	message = item + " is currently at a price " + price['lowest_price'] + "\n" + link
+	await ctx.channel.send(message)
 
 @client.command()
 async def test(ctx, weapon) :
--- searchFunction.py
+++ searchFunction.py
@@ -2,40 +2,38 @@ def replaceWeaponName(weapon) :
 	search = weapon.lower()
 	#rifles
 	if search.find("ak") != -1 or search.find("47") != -1 :
-		return "AK-47%20"
+		return "AK-47%20", "AK-47"
 	elif search.find("awp") != -1 :
-		return "AWP%20"
+		return "AWP%20", "AWP"
 	elif search.find("a1s") != -1 :
-		return "M4A1-S%20"
+		return "M4A1-S%20", "M4A1-S"
 	elif search.find("m4") != -1 :
-		return "M4A4%20"
+		return "M4A4%20", "M4A4"
 	elif search.find("ssg") != -1 or search.find("08") != -1 or search.find("scout") :
-		return "SSG%2008%20"
+		return "SSG%2008%20", "SSG 08"
 	elif search.find("scar") != -1 or search.find("20") != -1 :
-		return "SCAR-20%20"
+		return "SCAR-20%20", "SCAR-20"
 	elif search.find("g3") != -1 or search.find("sg1") != -1 :
-		return "G3SG1%20"
+		return "G3SG1%20", "G3SG1"
 	elif search.find("aug") != -1 :
-		return "AUG%20"
+		return "AUG%20", "AUG"
 	elif search.find("sg") != -1 or search.find("553") != -1 :
-		return "SG%20553%20"
+		return "SG%20553%20", "SG 553"
 	elif search.find("galil") != -1 or search.find("ar") != -1 :
-		return "Galil%20AR%20"
+		return "Galil%20AR%20", "Galil AR"
 	elif search.find("famas") != -1 :
-		return "FAMAS%20"
-	elif search.find("g3") or search.find("sg1") != -1 :
-		return "G3SG1%20"
+		return "FAMAS%20", "FAMAS"
 	#SMGs
 
 def replaceWeaponWear(wear) :
 	search = wear.lower()
 	if search.find("fn") != -1 :
-		return "%28Factory%20New%29"
+		return "%28Factory%20New%29", "(Factory New)"
 	elif search.find("mw") != -1 :
-		return "%28Minimal%20Wear%29"
+		return "%28Minimal%20Wear%29", "(Minimal Wear)"
 	elif search.find("ft") != -1 :
-		return "%28Field-Tested%29"
+		return "%28Field-Tested%29", "(Field-Tested)"
 	elif search.find("ww") != -1 :
-		return "%28Well-Worn%29"
+		return "%28Well-Worn%29", "(Well-Worn)"
 	elif search.find("bs") != -1 :
-		return "%28Battle-Scarred%29"
+		return "%28Battle-Scarred%29", "(Battle-Scarred)"
